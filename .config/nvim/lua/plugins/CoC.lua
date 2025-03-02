4e85609ada7e6793fc29737c9319bc3b770fa419
Lucas Yamanishi <lucas.yamanishi@gmail.com>
2025-03-02 16:23:21 -0500

    nvim: Updates for CoC
    * Add several langage server extensions
    * Fix tab completion

--- .config/nvim/lua/plugins/CoC.lua
+++ .config/nvim/lua/plugins/CoC.lua
@@ -5,33 +5,46 @@ return {
         event = "BufEnter",
         config = function()
             vim.g.coc_global_extensions = {
+                '@yaegassy/coc-ansible',
+                'coc-copilot',
+                'coc-diagnostic',
+                'coc-docker',
                 'coc-eslint',
+                'coc-git',
                 'coc-go',
+                'coc-golines',
                 'coc-json',
                 'coc-lua',
                 'coc-markdownlint',
+                'coc-markdown-preview-enhanced',
                 'coc-pairs',
                 'coc-powershell',
                 'coc-prettier',
                 'coc-pyright',
+                'coc-rls',
                 'coc-rust-analyzer',
                 'coc-sh',
                 'coc-solargraph',
+                'coc-spell-checker',
+                'coc-sql',
                 'coc-swagger',
                 'coc-toml',
                 'coc-tsserver',
                 'coc-vimlsp',
-                'coc-yaml'
+                'coc-xml',
+                'coc-yaml',
             }
-            --[[
-            " CoC autocompletion
-            inoremap <silent><expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
-            " vim.api.nvim_set_keymap('i', '<S-Tab>', '<C-p>', {silent= true, expr = true})
-            inoremap <expr> <C-j> pumvisible() ? "\<C-n>" : "\<C-j>"
-            " vim.api.nvim_set_keymap('i', '<C-j>', '<C-n>', {expr = true})"
-            --]]
-            vim.api.nvim_set_keymap('i', '<S-Tab>', '<C-p>', {silent= true, expr = true})
-            vim.api.nvim_set_keymap('i', '<C-j>', '<C-n>', {expr = true})
+            function vim.fn.check_back_space()
+                local col = vim.fn.col('.') - 1
+                return col == 0 or vim.fn.getline('.')[col]:match('%s')
+            end
+
+            vim.api.nvim_set_keymap(
+                'i',
+                '<S-Tab>',
+                'pumvisible() ? "\\<C-n>" : v:lua.vim.fn.check_back_space() ? "\\<Tab>" : "\\<C-p>"',
+                { noremap = true, expr = true, silent = true }
+            )
         end,
     }
 }
