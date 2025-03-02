39ba2b5477e26c95493cbc8195d8084f62f580ae
Lucas Yamanishi <lucas.yamanishi@gmail.com>
2025-03-02 16:24:11 -0500

    nvim: Remove useless comments from copilot.lua

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -12,15 +12,6 @@ return {
     build = 'make tiktoken',
     opts = {
       -- See Configuration section for options
-      --[[
-      mappings = {
-        send = {
-          insert = '<C-CR>',
-          normal = '<C-CR>',
-	    },
-      },
-      --]]
-      chat_autocomplete = true,
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
