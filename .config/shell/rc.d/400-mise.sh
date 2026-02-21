3f79ef44288c73563d416f31a5da47f4d7f03338
Lucas Yamanishi <lucas.yamanishi@gmail.com>
2026-02-21 01:16:23 -0500

    feat(mise): Enable tab completion

--- .config/shell/rc.d/400-mise.sh
+++ .config/shell/rc.d/400-mise.sh
@@ -12,7 +12,9 @@
 if command -v mise > /dev/null 2>&1; then
 	if [ -n "$BASH_VERSION" ]; then
 		eval "$(mise activate bash)"
+		eval "$(mise completions bash)"
 	elif [ -n "$ZSH_VERSION" ]; then
 		eval "$(mise activate zsh)"
+		eval "$(mise completions zsh)"
 	fi
 fi
