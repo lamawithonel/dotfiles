1dde3324dd1184bfb4cbaea36ff0f281ca9d31f0
Lucas Yamanishi <lucas.yamanishi@gmail.com>
2026-03-06 22:48:46 -0500

    feat(nvim): Customize scrolling with Snacks

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -10,7 +10,7 @@ return {
         "fenced_code_block",
         "code_fence_content",
         "code_block",
-        "block_continuation", -- This is the one appearing in your tree
+        "block_continuation",
         "fenced_code_block_delimiter",
       }
 
@@ -21,6 +21,25 @@ return {
       -- 2. Disable active scope lines
       opts.scope.exclude = opts.scope.exclude or {}
       opts.scope.exclude.node_types = ignored_nodes
+
+      opts.scroll = {
+        animate = {
+          duration = { step = 10, total = 100 },
+          easing = "inCubic",
+        },
+        -- faster animation when repeating scroll after delay
+        animate_repeat = {
+          delay = 100, -- delay in ms before using the repeat animation
+          duration = { step = 5, total = 50 },
+          easing = "linear",
+        },
+        -- what buffers to animate
+        filter = function(buf)
+          return vim.g.snacks_scroll ~= false
+            and vim.b[buf].snacks_scroll ~= false
+            and vim.bo[buf].buftype ~= "terminal"
+        end,
+      }
     end,
   },
 }
