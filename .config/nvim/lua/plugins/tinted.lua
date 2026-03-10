f2f4f118f30b25304a0ac9c64cfd793ba344f9a1
Lucas Yamanishi <lucas.yamanishi@gmail.com>
2026-03-10 15:01:18 -0400

    fix(nvim/tinted.nvim): Remove extraneous color setup

--- .config/nvim/lua/plugins/tinted.lua
+++ .config/nvim/lua/plugins/tinted.lua
@@ -1,20 +1,8 @@
 return {
   "tinted-theming/tinted-nvim",
+  priority = 1000,
   lazy = false,
   config = function()
-    local t_Co = tonumber(vim.fn.system("tput colors || echo 8"))
-
-    if vim.opt.termguicolors or vim.env.COLORTERM == "truecolor" or t_Co >= 256 then
-      vim.opt.termguicolors = true
-      vim.g.base16colorspace = 256
-    elseif t_Co >= 88 then
-      vim.g.base16colorspace = 88
-    elseif t_Co >= 16 then
-      vim.g.base16colorspace = 16
-    else
-      vim.g.base16colorspace = 8
-    end
-
     require("tinted-colorscheme").setup()
   end,
 }
